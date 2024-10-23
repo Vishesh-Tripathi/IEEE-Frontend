72339fd3ac0be30e97b41ae7c8fc382c5266ce68
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-10-24 00:12:53 +0530

    Update Event.jsx

--- src/Pages/Events/Event.jsx
+++ src/Pages/Events/Event.jsx
@@ -10,7 +10,7 @@ const Carousel = () => {
 
   const fetched = async() => {
     // Fetch images dynamically from backend
-    const res = await axios.get('https://ieee-backend-tu8h.onrender.com/highlights/getimages');
+    const res = await axios.get('https://ieee-backend-rgzb.vercel.app/highlights/getimages');
     setImageSet(res.data);
     console.log(res.data);
   };
