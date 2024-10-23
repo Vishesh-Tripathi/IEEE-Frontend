226b4a203414871d072e8f953bcea611213d2ab0
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-10-24 00:09:53 +0530

    Update Team.jsx

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -10,7 +10,7 @@ const InstructorCard = () => {
 
   const fetchTeamMembers = async () => {
     try {
-      const response = await axios.get('https://ieee-backend-tu8h.onrender.com/teams');
+      const response = await axios.get('https://ieee-backend-rgzb.vercel.app/teams');
       setInstructors(response.data);
     } catch (error) {
       console.error('Error fetching team members:', error);
